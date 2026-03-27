1f7839cbae484c919bece4eb64189dbba703dd3b
jeffschoe <ratacame@protonmail.com>
2026-03-27 19:46:13 -0400

    Created db connection via pool in db/index.ts

--- src/server/config/env.ts
+++ src/server/config/env.ts
@@ -1,12 +1,19 @@
-import"dotenv/config"; 
-//installs the dotenv library, which loads environment variables from a .env file
-// it reads the .env file and injects values into process.env
-// so now we can use process.env.DATABASE_URL
+import "dotenv/config"; 
+/*
+installs the dotenv library, which loads environment variables from a .env file
+it reads the .env file and injects values into process.env
+so now we can use process.env.DATABASE_URL
+
+This file runs once when your app starts (when it’s first imported)
+If the env var is missing → app crashes immediately
+If it exists → the rest of your app can safely assume it's valid
+*/
 if (!process.env.DATABASE_URL) { //can't find DB_URL
   throw new Error("DATABASE_URL is not set")
 }
-//this forces use to confirm it exists at startup
+//this forces us to confirm it exists at startup
 
 export const env = {
   DATABASE_URL: process.env.DATABASE_URL,
+  //make db string availble to rest of program
 }
