393a3c961a40a1e7effea1c2666d63ef6745ff8f
jeffschoe <ratacame@protonmail.com>
2026-04-01 16:27:30 -0400

    Added a server health check endpoint

---
+++ src/server/routes/health.routes.ts
@@ -0,0 +1,13 @@
+import { Express } from "express";
+
+
+
+export function registerHealthRoutes(app: Express) {
+  
+  app.get('/api/healthz', (_req, res) => {
+    res.status(200).json({
+      status: 'ok',
+    });
+  });
+
+}
