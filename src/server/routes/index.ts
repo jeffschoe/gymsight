9ad4170bc318e5c8a6269fb549a34e8150004163
jeffschoe <ratacame@protonmail.com>
2026-04-01 21:31:01 -0400

    Added db readiness route

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,13 @@
 import { Express } from "express";
 import { registerUserRoutes } from "../modules/users/users.routes.js";
 import { registerHealthRoutes } from "./health.routes.js";
+import { registerReadyRoutes } from "./ready.routes.js";
 
 
 export function registerRoutes(app: Express) {
 
-  registerHealthRoutes(app); //is the server alive?
+  registerHealthRoutes(app); //is server alive?
+  registerReadyRoutes(app); //is db ready serve traffic?
 
   registerUserRoutes(app);
   // registerWorKOrderRoutes(app); FUTURE ONLY
---
+++ src/server/routes/ready.routes.ts
@@ -0,0 +1,27 @@
+import { Express } from "express";
+import { db } from "../db/index.js";
+
+
+
+
+export function registerReadyRoutes(app: Express) {
+  
+  app.get('/api/readyz', async (_req, res) => {
+
+    try {
+      await db.execute('select 1');
+
+      return res.status(200).json({
+        status: 'ok',
+        db: 'connected',
+      });
+    } catch {
+      return res.status(500).json({
+        status: 'error',
+        db: 'disconnected'
+      });
+    }
+    
+  });
+
+}
