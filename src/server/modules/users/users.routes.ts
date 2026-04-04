a3ba233420dd70d6fdae33378fc03ce3f190fb66
jeffschoe <ratacame@protonmail.com>
2026-04-04 00:39:18 -0400

    updated GET /api/users route to have RBAC

--- src/server/modules/users/users.routes.ts
+++ src/server/modules/users/users.routes.ts
@@ -2,13 +2,14 @@
 import { Express } from 'express';
 import * as userController from './users.controller.js'
 import { middlewareRequireAuth } from '../../middleware/auth.middleware.js';
+import { middlewareRequireRole } from '../../middleware/rbac.middleware.js';
 
 
 
 export function registerUserRoutes(app: Express) {
 
   app.post('/api/users', userController.createUser);
-  app.get('/api/users', middlewareRequireAuth, userController.getUsers);
+  app.get('/api/users', middlewareRequireAuth, middlewareRequireRole("admin"), userController.getUsers);
   //app.get('/api/users/:id', userController.getUser);
   //app.put('/api/users/:id, userController.updateUser);
   app.delete('/api/users/:id', middlewareRequireAuth, userController.deleteUserById);
